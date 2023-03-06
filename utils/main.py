cf5e3f1afd193140c4da4ac305c6b639b2631bbe
Rocksed <ilyagotin@mail.ru>
2023-03-06 12:09:44 +0300

    Inheritance. Homework

--- utils/main.py
+++ utils/main.py
@@ -1,112 +1,89 @@
-import json
 import os
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 
 
-class Channel:
+class Video:
     YOUTUBE_API_SERVICE_NAME = 'youtube'
     YOUTUBE_API_VERSION = 'v3'
 
-    def __init__(self, channel_id):
-        self._id = channel_id
+    def __init__(self, video_id):
+        self._id = video_id
         self._title = None
-        self._description = None
-        self._url = None
-        self._subscriber_count = None
-        self._video_count = None
         self._view_count = None
+        self._like_count = None
         self._get_data()
 
     @staticmethod
     def get_service():
-        load_dotenv()  # load API key from .env file
+        load_dotenv()
         api_key = os.getenv('YOUTUBE_API_KEY')
-        return build(Channel.YOUTUBE_API_SERVICE_NAME, Channel.YOUTUBE_API_VERSION, developerKey=api_key)
-
-    @property
-    def channel_id(self):
-        return self._id
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def description(self):
-        return self._description
-
-    @property
-    def url(self):
-        return self._url
-
-    @property
-    def subscriber_count(self):
-        return self._subscriber_count
-
-    @property
-    def video_count(self):
-        return self._video_count
-
-    @property
-    def view_count(self):
-        return self._view_count
+        return build(Video.YOUTUBE_API_SERVICE_NAME, Video.YOUTUBE_API_VERSION, developerKey=api_key)
 
     def _get_data(self):
-        youtube = Channel.get_service()
-        response = youtube.channels().list(
+        youtube = Video.get_service()
+        response = youtube.videos().list(
             part='snippet,statistics',
             id=self._id
         ).execute()
         if response.get('items'):
             data = response['items'][0]
             self._title = data['snippet']['title']
-            self._description = data['snippet']['description']
-            self._url = f"https://www.youtube.com/channel/{self._id}"
-            self._subscriber_count = int(data['statistics']['subscriberCount'])
-            self._video_count = int(data['statistics']['videoCount'])
             self._view_count = int(data['statistics']['viewCount'])
+            self._like_count = int(data['statistics']['likeCount'])
         else:
-            raise ValueError(f"No channel data found for channel ID {self._id}")
+            raise ValueError(f"No video data found for video ID {self._id}")
 
-    def to_json(self, filename):
-        data = {
-            'channel_id': self._id,
-            'title': self._title,
-            'description': self._description,
-            'url': self._url,
-            'subscriber_count': self._subscriber_count,
-            'video_count': self._video_count,
-            'view_count': self._view_count
-        }
+    @property
+    def title(self):
+        return self._title
 
-        with open(filename, 'w') as file:
-            json.dump(data, file)
+    @property
+    def view_count(self):
+        return self._view_count
 
-        print(f"Data saved in {os.path.abspath(filename)}")
+    @property
+    def like_count(self):
+        return self._like_count
 
     def __str__(self):
-        return f"YouTube channel: {self._title}"
+        return f"{self._title}"
 
     def __repr__(self):
-        return f"Channel('{self._id}')"
-
-    def __add__(self, other):
-        return self._subscriber_count + other._subscriber_count
-
-    def __lt__(self, other):
-        return self._subscriber_count < other._subscriber_count
-
-    def __gt__(self, other):
-        return self._subscriber_count > other._subscriber_count
+        return f"Video('{self._id}')"
 
 
-ch1 = Channel('vDud')
-ch2 = Channel('edit')
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        self._playlist_name = None
+        self._playlist_id = playlist_id
+        super().__init__(video_id)
 
-print(ch1)  # YouTube channel: vDud
-print(ch2)  # YouTube channel: edit
+    def _get_data(self):
+        super()._get_data()
+        youtube = Video.get_service()
+        response = youtube.playlists().list(
+            part='snippet',
+            id=self._playlist_id
+        ).execute()
+        if response.get('items'):
+            data = response['items'][0]
+            self._playlist_name = data['snippet']['title']
+        else:
+            raise ValueError(f"No playlist data found for playlist ID {self._playlist_id}")
 
-print(ch1 + ch2)  # 13940000
+    @property
+    def playlist_name(self):
+        return self._playlist_name
 
-print(ch1 < ch2)
+    def __str__(self):
+        return f"{self._title} ({self._playlist_name})"
+
+    def __repr__(self):
+        return f"PLVideo('{self._id}', '{self._playlist_id}')"
+
+
+video1 = Video('9lO06Zxhu88')
+video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+print(video1)
+print(video2)
